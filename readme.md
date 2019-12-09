bbbf7ae6e1c8fecca76407f6afa8a365a881c157
wangchen <jimmychen21@163.com>
2019-12-09 16:29:40 +0800

    文件结构

---
+++ readme.md
@@ -0,0 +1,3 @@
+## React webapp scaffold
+
+- React / Webpack4 / Babel 7 / Less / Eslint / for webapp
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Index = () => <h5> React / Webpack4 / Babel 7 / Less </h5>
+
+ReactDOM.render(<Index />, document.getElementById('index'))
